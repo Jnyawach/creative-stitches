04ceb40d6b1faf7de14ed62a5afe036ed430a018
jnyawach <nyawach41@gmail.com>
2023-03-12 06:48:34 +0300

    subs

--- resources/views/components/user/subscribe.vue
+++ resources/views/components/user/subscribe.vue
@@ -14,7 +14,10 @@
                </div>
                <div>
                   <button @click="hide">
-                      <span class="text-4xl"><i class="fal fa-times"></i></span>
+                      <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+
                   </button>
                </div>
            </div>
