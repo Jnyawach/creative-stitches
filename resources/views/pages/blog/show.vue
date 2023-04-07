90dd681823088a68faae563f665fdf0a9cc96f3d
jnyawach <nyawach41@gmail.com>
2023-04-07 17:50:03 +0300

    tailwind-typography

--- resources/views/pages/blog/show.vue
+++ resources/views/pages/blog/show.vue
@@ -18,7 +18,8 @@
         </div>
         <div v-html="post.data.content"
              class="prose-lg prose-headings:font-bold
-             prose-headings:text-gray-900 prose-text-gray-500 prose-a:text-teal-700">
+             prose-headings:text-gray-900 prose-text-gray-500 prose-a:text-teal-700
+             prose-ul:list-disc">
         </div>
         <p><span class="text-teal-700 mr-2">Tags:</span>{{post.data.tags}}</p>
         <div class="flex mt-2 gap-3">
